4d1dcbcf90160fabdb40bc6f6450a0ca91411807
maxidl <maximilian@ellamind.com>
2025-11-01 12:32:16 +0100

    fix ntasks per node

--- run_nccl_tests.sh
+++ run_nccl_tests.sh
@@ -13,5 +13,5 @@ srun \
     --ntasks=$ntasks \
     --cpus-per-task=4 \
     --gpus=$ntasks \
-    --ntasks-per-node=4 \
+    --ntasks-per-node=$gpus_per_node \
     ./nccl-tests/build/all_reduce_perf -b 8 -e 8G -f 2 -g 1
